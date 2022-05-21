eaf9607d49675d91cc106a84b577981dcdd0fb05
Diego Oliveira da Silva <diegolinkk@gmail.com>
2022-05-21 12:17:06 -0300

    Criação da tabela conceitos

--- database/migrations/2022_05_21_121343_create_conceitos_table.php
+++ database/migrations/2022_05_21_121343_create_conceitos_table.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// - conceitos
+//   - id
+//   - nome
+//   - descrição
+//   - usuário (fk)
+//   - projetos(fk many to many)
+
 return new class extends Migration
 {
     /**
@@ -13,16 +20,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('projetos', function (Blueprint $table) {
+        Schema::create('conceitos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string("nome");
             $table->string("descricao");
-            $table->boolean("feito")->default(false);
             $table->integer("user_id");
             $table->foreign("user_id")
-                ->references('id')
-                ->on('users');
+                ->references("id")
+                ->on("users");
+
         });
     }
 
@@ -33,6 +40,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projetos');
+        Schema::dropIfExists('conceitos');
     }
 };
